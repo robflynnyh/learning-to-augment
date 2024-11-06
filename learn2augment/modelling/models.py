14388125be43c615e2131eb62c4d378d508d21b8
Rob Flynn <rjflynn2@shef.ac.uk>
2024-11-06 15:46:20 +0000

    Basic value network

--- learn2augment/modelling/models.py
+++ learn2augment/modelling/models.py
@@ -46,3 +46,35 @@ class Policy(base):
         x = self.network(x)
         x = x.sigmoid()
         return x
+    
+
+class Value(base):
+    def __init__(
+            self,
+            input_dim,
+            output_dim,
+            **kwargs
+        ) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        self.state_size = kwargs.get("state_dim", 80)
+        
+        self.initial_state = nn.Parameter(torch.empty((1, self.state_size)))
+        nn.init.uniform_(self.initial_state, a=-0.1, b=0.1)
+
+        self.network = nn.Sequential(
+            SwiGlu(input_dim + self.state_size, output_dim=input_dim),
+            SwiGlu(input_dim, output_dim),
+        )
+
+    def get_initial_state(self, batch_size):
+        return self.initial_state.repeat(batch_size, 1)
+
+    def forward(self, x:Tensor, state: Tensor | None = None): # x = action from policy
+        if state is None:
+            state = self.get_initial_state(x.size(0))
+        x = torch.cat([state, x], dim=-1)
+        x = self.network(x)
+        predicted_reward, next_state = x.split(split_size_or_sections=[1, self.state_size], dim=-1)
+        return predicted_reward, next_state
+
