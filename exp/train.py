9ffc326a62e0b4fe2d6dccaa39f6a5b84d0d0f7a
Rob Flynn <rjflynn2@shef.ac.uk>
2024-11-08 14:48:11 +0000

    start to setup training script (all boilerplate)

---
+++ exp/train.py
@@ -0,0 +1,17 @@
+import argparse
+from omegaconf.omegaconf import OmegaConf
+
+def main(config):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-config", type=str, required=True, help="Path to YAML config file")
+    args = parser.parse_args()
+    config = OmegaConf.load(args.config)
+    main(config)
+
+
+
+
