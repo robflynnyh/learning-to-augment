4e4eee1a192536626c11e3e0c93a20a12ed4049b
Rob Flynn <rjflynn2@shef.ac.uk>
2024-11-12 13:56:14 +0000

    working on cpu rollout

--- l2augment/rollout/cpu.py
+++ l2augment/rollout/cpu.py
@@ -19,9 +19,6 @@ def cpu_rollout(
     ):
 
     audio_chunks = chunk_audio_function(spec = audio)
-    encoded_text_chunks = [torch.LongTensor(tokenizer.encode(text_chunk))[None] for text_chunk in text_chunks]
-    encoded_text_chunks_lengths = torch.LongTensor([el.shape[0] for el in encoded_text_chunks])
-    assert len(audio_chunks) == len(encoded_text_chunks), 'length mismatch'
     utterances = len(audio_chunks)
 
     indexes = list(range(utterances))
@@ -39,20 +36,20 @@ def cpu_rollout(
     for current_index in indexes:
         audio_sample = audio_chunks[current_index] 
         augmented_audio_sample = policy.augment(audio_sample)
-        audio_signal = torch.cat([
-            audio_sample,
-            augmented_audio_sample         
-        ])
+        # audio_signal = torch.cat([
+        #     audio_sample,
+        #     augmented_audio_sample         
+        # ])
 
-        out = asr_model(audio_signal = audio_signal)
-        pseudo_targets = decoder(out['final_posteriors'][0].detach())
-        noisy_predictions = out['final_posteriors'][1]
+        # out = asr_model(audio_signal = audio_signal)
+        # pseudo_targets = decoder(out['final_posteriors'][0].detach())
+        # noisy_predictions = out['final_posteriors'][1]
 
-        if verbose: print(pseudo_targets, decoder(noisy_predictions.detach()))
+        # if verbose: print(pseudo_targets, decoder(noisy_predictions.detach()))
 
-        pseudo_targets = torch.LongTensor(tokenizer.encode(pseudo_targets)).unsqueeze(0)
-        N, B = noisy_predictions.shape[1], noisy_predictions.shape[0]
-        total_tokens_in_loss = N * B
+        # pseudo_targets = torch.LongTensor(tokenizer.encode(pseudo_targets)).unsqueeze(0)
+        # N, B = noisy_predictions.shape[1], noisy_predictions.shape[0]
+        # total_tokens_in_loss = N * B
 
     #     loss = ctc_loss_fn(noisy_predictions.transpose(0, 1), pseudo_targets, torch.LongTensor([N] * noisy_predictions.shape[0]), torch.LongTensor([pseudo_targets.shape[1]] * pseudo_targets.shape[0])) / total_tokens_in_loss
         
